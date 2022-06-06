919f1bd9f0ef3303e25bb3a28541fe8b92c89ded
emreyldz01 <emreyldz188@gmail.com>
2022-06-06 20:09:35 +0300

    odev2-Bitti

---
+++ odev2/template.js
@@ -0,0 +1,20 @@
+let input = prompt("enter a text");
+console.log(input);
+//girilen string boyutunda bir char array oluşturdum.
+var charArr=[input.toString().length];
+
+for (var i = 0; i < input.length; i++) {
+    // her elemanı diziye aktarıyoruz
+    charArr[i] = input.charAt(i);
+}
+// başlangıç olarak ilk index yerine ikiciyi 
+// bitiş yerini son index yerine sondan bir önceki seçtim 
+for(var i = 1; i < charArr.length-1; i++)
+{
+    // ve başlangıç ile son arasındaki indexlerin değerlerini * ile değiştirdim.
+    charArr[i]="*";
+}
+console.log(text);
+//virgülleri null değerle değiştirdim bu sayede düzgün bir kelime olarak görünecek
+let text = charArr.join("");
+console.log(text);
