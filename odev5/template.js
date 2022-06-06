8afad62ebecccdd15b7aa2a3012515dd1ee6c26f
emreyldz01 <emreyldz188@gmail.com>
2022-06-06 22:15:09 +0300

    odevlerde guncelleme odev5=> done

---
+++ odev5/template.js
@@ -0,0 +1,10 @@
+alert("2 farklı tam sayı giriniz");
+var x = prompt("x değerini giriniz");
+var k = prompt("k değerini giriniz");
+
+function problemSolver(x,k){
+    var result = ((3*x)+(4*k));
+    alert(result);
+}
+
+problemSolver(x,k);
